0fd0761b927f0f3981c1d328e3f0b1673d94965b
_An_Jin_ <aji0719@gmail.com>
2022-06-17 11:22:21 +0900

    1.2

--- intro/pages/index.js
+++ intro/pages/index.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
-    <h2>Hello</h2>
-  )
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
